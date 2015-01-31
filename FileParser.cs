581bd49722252897d9f9e3493bba0600dbae4fb5
blakar <blakar@post.cz>
2015-01-31 00:05:12 -0800

    Add parser of the link files

---
+++ FileParser.cs
@@ -0,0 +1,109 @@
+﻿namespace Blakar.Tools.LinkDb
+{
+    using System.IO;
+    using System;
+    using System.Collections.Generic;
+
+    internal class FileParser
+    {
+        private string fileName;
+        //private List<string> tags;
+        //private List<string> links;
+        private HashSet<string> tags;
+        private HashSet<string> links;
+
+        public FileParser(string fileName)
+        {
+            this.fileName = fileName;
+            //this.tags = new List<string>();
+            this.tags = new HashSet<string>();
+            this.links = new HashSet<string>();
+        }
+
+        public int TagCount
+        {
+            get
+            {
+                return tags.Count;
+            }
+        }
+
+        public int LinkCount
+        {
+            get
+            {
+                return links.Count;
+            }
+        }
+
+        public void Parse()
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                string line = null;
+                do
+                {
+                    line = sr.ReadLine();
+                    this.ProcessFileLine(line);
+                }
+                while (line != null);
+            }
+        }
+
+        private void ProcessFileLine(string line)
+        {
+            if (line != null)
+            {
+                if (line.StartsWith("%%") || string.IsNullOrEmpty(line))
+                {
+                    // this is a comment; ignore it
+                }
+                else if (line.StartsWith("."))
+                {
+                    var command = SplitDotCommand(line);
+                    this.ProcessCommand(command);
+                }
+            }
+        }
+
+        private void ProcessCommand(Tuple<string, string> command)
+        {
+            var commandType = command.Item1;
+            var commandArg = command.Item2;
+
+            switch (commandType.ToLowerInvariant())
+            {
+                case ".tag":
+                    this.tags.Add(commandArg);
+                    break;
+
+                case ".link":
+                    this.links.Add(commandArg);
+                    break;
+            }
+        }
+
+        private Tuple<string, string> SplitDotCommand(string line)
+        {
+            // assumes line starts with a dot, find next blank line
+            int pos = line.IndexOf(" ");
+            if (pos > -1)
+            {
+                return new Tuple<string, string>(
+                    line.Substring(0, pos), 
+                    line.Substring(pos + 1));
+            }
+            else
+            {
+                return new Tuple<string, string>(line.Trim(), null);
+            }
+        }
+    }
+
+    internal class LinkInfo
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Added { get; set; } // TODO: Change this
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,57 @@
 ﻿namespace Blakar.Tools.LinkDb
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using PowerArgs;
 
+    /// <summary>
+    /// Entry point to the LinkDb application
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Starts executing application with user-specified arguments 
+        /// </summary>
+        /// <param name="args">List of arguments passed from external environment</param>
         public static void Main(string[] args)
         {
+            Args.InvokeAction<LinkDbArgs>(args);
         }
+    }
+
+    /// <summary>
+    /// Represents complete list of arguments supported by LinkDB console application
+    /// </summary>
+    [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+    internal class LinkDbArgs
+    {
+        /// <summary>
+        /// Handle help request
+        /// </summary>
+        [HelpHook, ArgShortcut("-?"), ArgDescription("Show this help")]
+        public bool Help { get; set; }
+
+        /// <summary>
+        /// Handle 'check' action.
+        /// </summary>
+        /// <param name="args">Check action arguments</param>
+        [ArgActionMethod, ArgDescription("Check linkdb file syntax")]
+        public void Check(CheckArguments args)
+        {
+            var parser = new FileParser(args.FileName);
+            parser.Parse();
+            Console.WriteLine("Counts: Tags={0}; Links={1}", parser.TagCount, parser.LinkCount);
+        }
+    }
+
+    /// <summary>
+    /// Arguments for the 'check' action.
+    /// </summary>
+    internal class CheckArguments
+    {
+        /// <summary>
+        /// Input file to process
+        /// </summary>
+        [ArgRequired, ArgShortcut("-f"), ArgDescription("Path to linkdb file"), ArgPosition(1)]
+        public string FileName{ get; set; }
     }
 }
