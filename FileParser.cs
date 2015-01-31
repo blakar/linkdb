8a706a0dbc5eed5b64fea59483d970b697fafd2b
blakar <blakar@post.cz>
2015-01-31 08:04:52 -0800

    Handle dates and 'added' command type

--- FileParser.cs
+++ FileParser.cs
@@ -7,17 +7,18 @@
     internal class FileParser
     {
         private string fileName;
-        //private List<string> tags;
-        //private List<string> links;
+
         private HashSet<string> tags;
         private HashSet<string> links;
+        private LinkInfo currentLinkInfo;
+        private List<LinkInfo> linkDetails;
 
         public FileParser(string fileName)
         {
             this.fileName = fileName;
-            //this.tags = new List<string>();
             this.tags = new HashSet<string>();
             this.links = new HashSet<string>();
+            this.linkDetails = new List<LinkInfo>();
         }
 
         public int TagCount
@@ -87,6 +88,24 @@
                         this.WriteError("Duplicate link: " + commandArg);
                     }
                     this.links.Add(commandArg);
+                    this.currentLinkInfo = new LinkInfo { Link = commandArg };
+                    this.linkDetails.Add(this.currentLinkInfo);
+                    break;
+
+                case ".added":
+                    var date = ConvertCompactDateStringToDateTime(commandArg);
+                    if (date != DateTime.MinValue)
+                    {
+                        this.currentLinkInfo.Added = date;
+                    }
+                    else
+                    {
+                        this.WriteError("Added command contained invalid date: " + commandArg);
+                    }
+                    break;
+
+                default:
+                    this.WriteError("Unknown command type: " + commandType);
                     break;
             }
         }
@@ -107,6 +126,33 @@
             }
         }
 
+        private static DateTime ConvertCompactDateStringToDateTime(string date)
+        {
+            // date is in format YYYYMMDD
+            if (string.IsNullOrWhiteSpace(date))
+            {
+                return DateTime.MinValue;
+            }
+
+            date = date.Trim();
+            if (date.Length != 8)
+            {
+                return DateTime.MinValue;
+            }
+
+            int year = ConvertToInt(date.Substring(0, 4));
+            int month = ConvertToInt(date.Substring(4, 2));
+            int day = ConvertToInt(date.Substring(6, 2));
+
+            return new DateTime(year, month, day);
+        }
+
+        private static int ConvertToInt(string text, int defaultValue = 0)
+        {
+            int result;
+            return int.TryParse(text, out result) ? result : defaultValue;
+        }
+
         private void WriteError(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -119,6 +165,6 @@
     {
         public string Link { get; set; }
         public string Title { get; set; }
-        public string Added { get; set; } // TODO: Change this
+        public DateTime? Added { get; set; } 
     }
 }
