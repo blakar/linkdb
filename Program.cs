b3b5c53e81980f72f7841a7f5dbc2fb9fa28c7dc
blakar <blakar@post.cz>
2015-01-30 21:22:01 -0800

    Create project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Blakar.Tools.LinkDb
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
